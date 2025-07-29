0233df3c933e7fa8b20700c6b625ebf74d4a24ca
Utku Çakar <cakarutku1@hotmail.com>
2025-07-29 17:59:38 +0300

    created first test project

---
+++ UnitTestExample/UnitTestExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExample")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d9a4b16-f304-406b-a46a-be9caa4abebe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestExample/UnitTestExample/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestExample
+{
+    [TestClass] // TestClass attribute.
+    public class UnitTest1
+    {
+        [TestMethod] // TestMethod attribute.
+        public void TestMethod1()
+        {
+        }
+    }
+}
