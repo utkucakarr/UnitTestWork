b11a9a63a67ad87f14c85c5f165231e997776b3c
Utku Çakar <cakarutku1@hotmail.com>
2025-07-30 21:32:35 +0300

    work for aresame, Inconclusive, IsInstanceOfType and IsTrue

---
+++ UnitTestExample/AssertAreSame/AreSameWork.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AreSameWork.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AreSameWork.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cf19d891-d6fc-4383-9f7c-2809ab2c22c1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestExample/AssertAreSame/AreSameWork.Test/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AreSameWork.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var numbers = new byte[] { 1, 2, 3, };
+            var otherNumbers = numbers;
+            numbers[0] = 4;
+            otherNumbers[1] = 5;
+
+            // AreSame referanslara bakar
+            Assert.AreSame(numbers, otherNumbers);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int a = 10;
+            int b = a;
+
+            Assert.AreEqual(a, b, "AreEqual fail oldu");
+            Assert.AreSame(a, b, "AreSame fail oldu");
+            Assert.AreNotSame(a, b, "AreSame fail oldu");
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(1, 1);
+            // Hata mesajı fırlatır.
+            Assert.Inconclusive("Bu test yeterli değildir");
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var number = 5m;
+
+            // type doğrulama yapar.
+            Assert.IsInstanceOfType(number, typeof(decimal));
+            Assert.IsNotInstanceOfType(number, typeof(int));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.IsTrue(10 % 2 == 0);
+            Assert.IsFalse(10 % 2 == 1);
+        }
+    }
+}
