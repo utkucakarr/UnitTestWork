e5443b9671fb6495680b635986ba2cc83e92aa3a
Utku Çakar <cakarutku1@hotmail.com>
2025-07-31 00:16:33 +0300

    work for TestContext

---
+++ UnitTestExample/TestContext/TestContext.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestContext.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestContext.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e86c83f0-b80d-49a0-b510-33fd308e3f14")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestExample/TestContext/TestContext.Test/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestContextDemo.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // Runtime'da kendisi set olan bir nesne
+        public TestContext TestContext { get; set; }
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestContext.WriteLine("--TestInitialize--\n");
+            TestContext.WriteLine("Test Adı: {0}", TestContext.TestName);
+            TestContext.WriteLine("Test Durumu: {0}", TestContext.CurrentTestOutcome);
+            TestContext.WriteLine("Test Sınıfı: {0}", TestContext.FullyQualifiedTestClassName);
+            TestContext.Properties["information"] = "Bu bir ekstra bilgidir";
+
+            Assert.AreEqual(1, 1);
+        }
+
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestContext.WriteLine("--TestCleanup--\n");
+            TestContext.WriteLine("Test Adı: {0}", TestContext.TestName);
+            TestContext.WriteLine("Test Durumu: {0}", TestContext.CurrentTestOutcome);
+            TestContext.WriteLine("Test Sınıfı: {0}", TestContext.FullyQualifiedTestClassName);
+            TestContext.WriteLine("Test Bilgi: {0}", TestContext.Properties["information"]);
+
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestContext.WriteLine("--TestMethod1--\n");
+            TestContext.WriteLine("Test Adı: {0}", TestContext.TestName);
+            TestContext.WriteLine("Test Durumu: {0}", TestContext.CurrentTestOutcome);
+            TestContext.WriteLine("Test Sınıfı: {0}", TestContext.FullyQualifiedTestClassName);
+            TestContext.WriteLine("Test Bilgi: {0}", TestContext.Properties["information"]);
+
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
