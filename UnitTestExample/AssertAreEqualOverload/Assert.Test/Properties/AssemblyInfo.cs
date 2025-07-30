c64e827cf5cd4e8984de63ff246a26e55412bafc
Utku Çakar <cakarutku1@hotmail.com>
2025-07-30 21:08:33 +0300

    overload example for areequal

---
+++ UnitTestExample/AssertAreEqualOverload/Assert.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Assert.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assert.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("277e8542-113c-48df-a921-22c16cfca1db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestExample/AssertAreEqualOverload/Assert.Test/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Asserts.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            const double inputValue = 16;
+            const double expected = 4;
+            double actualed = Math.Sqrt(inputValue);
+
+            Assert.AreEqual(expected, actualed, "{0} sayısının karekökü {1} olmadılıdır", inputValue, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double expected = 3.1622;
+            // Formül: |beklenen-gerçekleşen|<=delta
+            // Virgülden sonra 4 basamağın doğru olmasını bekledik
+            double delta = 0.0001;
+
+            double actualed = Math.Sqrt(10);
+            Assert.AreEqual(expected, actualed, delta);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string expected = "MERHABA";
+            string actualed = "merhaba";
+
+            // Küçük büyük harf duyarsız oldu true ile
+            Assert.AreEqual(expected, actualed, true);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            const double unExpected = 0;
+            var actual = Math.Pow(5, 0);
+
+            Assert.AreNotEqual(unExpected, actual);
+        }
+    }
+}
