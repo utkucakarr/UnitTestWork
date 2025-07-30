f27772e96410add09cf89af2fd0e33a922fb7b85
Utku Çakar <cakarutku1@hotmail.com>
2025-07-30 21:59:22 +0300

    work for collection asserts

---
+++ UnitTestExample/CollectionAssert/CollectionAsserts.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CollectionAsserts.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CollectionAsserts.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("912c11d5-6353-466f-9089-6bdda53c25e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestExample/CollectionAssert/CollectionAsserts.Tests/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CollectionAsserts.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private List<string> _users;
+
+        [TestInitialize]
+        public void CreateData()
+        {
+            _users = new List<string>();
+            _users.Add("Utku");
+            _users.Add("Umut");
+            _users.Add("Nuray");
+        }
+
+        // Eleman ve sırası aynı olmadılır
+        [TestMethod]
+        public void Should_be_same_elements_and_order()
+        {
+            List<string> newUsers = new List<string>();
+
+            newUsers.Add("Utku");
+            newUsers.Add("Umut");
+            newUsers.Add("Nuray");
+
+            // Collection verilerini karşılaştırır.
+            CollectionAssert.AreEqual(newUsers, _users);
+        }
+
+        // Eleman aynı sırası farklı olmalı
+        [TestMethod]
+        public void Elemants_should_be_same_but_order_can_be_different()
+        {
+            List<string> newUsers = new List<string>();
+            newUsers.Add("Nuray");
+            newUsers.Add("Utku");
+            newUsers.Add("Umut");
+
+            CollectionAssert.AreEquivalent(_users, newUsers);
+        }
+
+
+        [TestMethod]
+        public void Should_be_same_elements_and_orderr()
+        {
+            List<string> newUsers = new List<string>();
+
+            newUsers.Add("Nuray");
+            newUsers.Add("Utku");
+            newUsers.Add("Umut");
+
+            // Collection verilerini karşılaştırır.
+
+            CollectionAssert.AreNotEqual(newUsers, _users);
+        }
+
+        [TestMethod]
+        public void Elemants_should_be_different()
+        {
+            List<string> newUsers = new List<string>();
+
+            newUsers.Add("Nuray");
+            newUsers.Add("Utku");
+            newUsers.Add("Umut");
+            newUsers.Add("Birol");
+
+            CollectionAssert.AreNotEquivalent(newUsers, _users);
+        }
+    }
+}
