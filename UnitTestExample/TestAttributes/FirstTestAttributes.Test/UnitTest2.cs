f2de65f274560f0cd6a19dd7640dd321aad65a0d
Utku Çakar <cakarutku1@hotmail.com>
2025-07-31 23:21:41 +0300

    work for Ingore, Timeout, Description attributes

---
+++ UnitTestExample/TestAttributes/FirstTestAttributes.Test/UnitTest2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace FirstTestAttributes.Test
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod, Description("Bu bir karekök testi yapan metottur")] // Açıklama
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod, Ignore] // Methodu Pass geçer
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod, Timeout(1000)] // 1 saniyenin üzerine çıkarsa timeout olsun ve test başarısız olsun 
+        public void TestMethod3()
+        {
+            Thread.Sleep(1500);
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod, Timeout(TestTimeout.Infinite)] // Sonsuz süre, 
+        public void TestMethod4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
