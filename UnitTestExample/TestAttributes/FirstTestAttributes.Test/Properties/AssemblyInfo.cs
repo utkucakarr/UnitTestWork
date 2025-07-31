cf89e7f865a98e2956851eb8df0adc7ad87f7260
Utku Çakar <cakarutku1@hotmail.com>
2025-07-31 23:13:21 +0300

    work for test attributes

---
+++ UnitTestExample/TestAttributes/FirstTestAttributes.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FirstTestAttributes.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FirstTestAttributes.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fe90f478-10dd-4e4f-8fda-4d95f4b1b7f1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestExample/TestAttributes/FirstTestAttributes.Test/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FirstTestAttributes.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [Owner("Utku")] // Kim yazdı
+        [TestCategory("Tester")]
+        [Priority(3)] // Öncelik sırası
+        [TestProperty("UpdatedBy", "Utku")] // Kendi gurbumuzu oluşturabiliriz
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [Owner("Utku")]
+        [TestCategory("Tester")]
+        [Priority(1)]
+        [TestProperty("UpdatedBy", "Utku")]
+        [TestProperty("UpdatedBy2", "Umut")]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [Owner("Utku")]
+        [TestCategory("Tester")]
+        [TestCategory("Developer")]
+        [TestCategory("Demo")]
+        [TestProperty("UpdatedBy", "Umut")]
+        [Priority(1)]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [Owner("Utku")]
+        [TestCategory("Developer")]
+        [Priority(2)]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [Owner("Umut")]
+        [TestCategory("Developer")]
+        [Priority(2)]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
